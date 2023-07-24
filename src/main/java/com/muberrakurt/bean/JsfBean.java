c5262d3c5c87bf507f3f521845e42a3eb3423a47
MuberraKurt <muberrakurt@ogr.eskisehir.edu.tr>
2023-07-24 14:28:22 +0300

    index.html JsfBean pom.xml modified

--- src/main/java/com/muberrakurt/bean/JsfBean.java
+++ src/main/java/com/muberrakurt/bean/JsfBean.java
@@ -1,20 +1,21 @@
 package com.muberrakurt.bean;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.annotation.ApplicationMap;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
 
 @Named(value="cdiBean")
-@ApplicationScoped
+@RequestScoped
 @Setter
 @Getter
 public class JsfBean {
+
     private String patika;
     public JsfBean() {
-        this.patika = "Spring Boot Eğitimi Hoşgeldiniz";
+        patika="Spring Boot Eğitime Hoşgeldiniz";
     }
-
 
 }
