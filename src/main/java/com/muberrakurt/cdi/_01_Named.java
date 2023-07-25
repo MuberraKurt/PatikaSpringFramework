1b55e588d09d33fb36aa23db5923495ae5530083
MuberraKurt <muberrakurt@ogr.eskisehir.edu.tr>
2023-07-25 16:05:46 +0300

    @Named modified

--- src/main/java/com/muberrakurt/cdi/_01_Named.java
+++ src/main/java/com/muberrakurt/cdi/_01_Named.java
@@ -22,19 +22,4 @@ public class _01_Named {
     private Long namedId;
     private String namedData=" Merhaba Named Cdi Bean";
 
-    public Long getNamedId() {
-        return namedId;
-    }
-
-    public void setNamedId(Long namedId) {
-        this.namedId = namedId;
-    }
-
-    public String getNamedData() {
-        return namedData;
-    }
-
-    public void setNamedData(String namedData) {
-        this.namedData = namedData;
-    }
 }
