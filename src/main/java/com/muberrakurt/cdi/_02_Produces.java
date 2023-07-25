6b7fdc06cae85da50c93b05a83399069a13ce154
MuberraKurt <muberrakurt@ogr.eskisehir.edu.tr>
2023-07-25 16:28:56 +0300

    @Produces @Inject

---
+++ src/main/java/com/muberrakurt/cdi/_02_Produces.java
@@ -0,0 +1,24 @@
+package com.muberrakurt.cdi;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Named;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "producetuto")
+@ApplicationScoped
+public class _02_Produces {
+
+    @Produces
+    public List<String> getList(){
+        List<String> liste=new ArrayList<>();
+        liste.add("Html5");
+        liste.add("css3");
+        liste.add("js");
+        liste.add("react");
+        liste.add("angular");
+        return liste;
+    }
+}
